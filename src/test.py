258c0a575047f3b1a07cb7358f5d43dafb022e0a
xabieralbizu2 <xabieralbizu8@gmail.com>
2024-11-26 19:58:58 +0100

    fotos

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from picamera2 import Picamera2
 
 def stream_video():
@@ -16,5 +17,24 @@ def stream_video():
             break
     cv2.destroyAllWindows()
 
+def capture():
+    picam = Picamera2()
+    picam.preview_configuration.main.size=(1280, 720)
+    picam.preview_configuration.main.format="RGB888"
+    picam.preview_configuration.align()
+    picam.configure("preview")
+    picam.start()
+    time.sleep(2)
+    cont=0
+    while True:
+        frame = picam.capture_array()
+        cv2.imshow("picam", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cont+=1
+            image = picam.capture_array()
+            cv2.imshow("Tablero capturado", image)
+            cv2.imwrite(f"chessboard{cont}.jpg", image)
+
 if __name__ == "__main__":
-    stream_video()
+    #stream_video()
+    capture()
